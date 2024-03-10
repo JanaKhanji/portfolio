47690f4c1f4b24493060f874be62936ecb7b68ac
jkhanji <jkhanji@turning.com>
2024-03-10 15:49:52 +0200

    Update firebase and about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,44 +1,44 @@
 import React from "react";
 
-function About(){
-    return (
-        <section className="section about-me">
-          <div className="container">
-            <div className="section-heading">
-              <h2>About Me</h2>
-              <div className="line-dec"></div>
-            </div>
-            <div className="left-image-post">
-              <div className="row">
-              
-                <div className="col-md-8">
-                  <div className="right-text">
-                    <h4>Hello, I'm Jana Khanji</h4>
-                    <p>
-                    Since I was little, I was amazed by the things a programmer could do with a computer,
-                    just by writing some code. and I always wondered about the things that I would be able to do
-                    when I grew up. Working hard and learning new stuff every day is a great way to achieve dreams,
-                    and now I know that I am where I always wanted to be, and I know that “little me” would be so proud.
-                    </p>
-                  </div>
-                </div>
-                <div className="col-md-4">
-                  <div className="left-image">
-                    <img src="assets/images/about.jpg" alt="aboutme" />
-                  </div>
-                </div>
+function About() {
+  return (
+    <section className="section about-me">
+      <div className="container">
+        <div className="section-heading">
+          <h2>About Me</h2>
+          <div className="line-dec"></div>
+        </div>
+        <div className="left-image-post">
+          <div className="row">
+
+            <div className="col-md-8">
+              <div className="right-text">
+                <h4>Hello, I'm Jana Khanji</h4>
+                <p className="pb-2">
+                Since I was little, I was amazed by the things a programmer could do with a computer, just by writing some code. and I always wondered about the things that I would be able to do when I grew up. Working hard and learning new stuff every day is a great way to achieve dreams, and now I know that I am where I always wanted to be, and I know that “little me” would be so proud.
+                </p>
+                <p className="pb-2">
+                Studying communications engineering expanded my perspective, allowing me to appreciate the connection of our modern world through technology. This educational background not only equipped me with technical skills but also sharpened my problem-solving abilities. By my second year in college, I discovered my true passion lies in web development.                </p>
               </div>
-                <p>
-                 Studying communications engineering gave me an opportunity to see the world in a different way with
-                  all the technologies that we have now. It also helped me build strong problem-solving skills.
-                  By my 2nd year in college, I realized that my #1 passion is web development.
-                </p>
-                <p>
-                Over the past few years, I have learned Angular, ReactJS, Ionic to build some personal and freelance projects. 
-                As a developer, I have always felt the desire to learn more and build more. 
-                </p>
+            </div>
+            <div className="col-md-4">
+              <div className="left-image">
+                <img src="assets/images/about.jpg" alt="aboutme" />
+              </div>
             </div>
           </div>
-        </section>)
+          <p  className="pb-2">
+            Over the past years, I've immersed myself in diverse software engineering projects, utilizing technologies like Angular, Bootstrap, Tailwind, and Java SpringBoot. Each project has been an opportunity to learn and grow, refining my expertise and expanding my capabilities.
+          </p>
+          <p  className="pb-1">
+            Working now as a software engineer, I find joy in turning ideas into reality through code. Whether it's building dynamic user interfaces or participating with a team to build a robust backend system,
+            I'm passionate about creating innovative solutions that make an impact
+          </p> 
+          <p>
+            Feel free to reach out to me via DM or email at janakhanji98@gmail.com.
+          </p>
+        </div>
+      </div>
+    </section>)
 }
 export default About;
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';   // for cloud firestore
 const firebaseConfig = {
-    apiKey: "AIzaSyD5kcdxhcxF08XTjSNSFoZnz633O4rMT0w",
-    authDomain: "janakhanji-portfolio.firebaseapp.com",
-    projectId: "janakhanji-portfolio",
-    storageBucket: "janakhanji-portfolio.appspot.com",
-    messagingSenderId: "639241714255",
-    appId: "1:639241714255:web:6902d020864d0bab3fc83a"
+  apiKey: "AIzaSyD5kcdxhcxF08XTjSNSFoZnz633O4rMT0w",
+  authDomain: "janakhanji-portfolio.firebaseapp.com",
+  projectId: "janakhanji-portfolio",
+  storageBucket: "janakhanji-portfolio.appspot.com",
+  messagingSenderId: "639241714255",
+  appId: "1:639241714255:web:6902d020864d0bab3fc83a"
   };
 
 firebase.initializeApp(firebaseConfig);
